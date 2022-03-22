971b1788765fc23817ed7a88bf3c389dfe33055c
raquel <M-Raquel@outlook.com.br>
2022-03-22 17:55:02 -0300

    styling modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import { Container } from './styles';
 
+import closeImg from '../../assets/close.svg'
+
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -14,6 +16,10 @@ export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionM
       overlayClassName = 'react-modal-overlay'
       className = 'react-modal-content'
     >
+      <button type="button" onClick={onRequestClose} className="react-modal-close">
+        <img src={closeImg} alt="botão de fechar modal" />
+      </button>
+
       <Container>
         <h2>Cadastrar Transação</h2>
 
