f7c7df463c1189d9b31563f2a8f70e0b480762a4
raquel <M-Raquel@outlook.com.br>
2022-03-29 17:42:12 -0300

    loading transactions using useContext

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Modal from 'react-modal';
 import { GlobalStyle } from "./styles/global";
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-import { TransactionsContext } from "./TransactionsContext";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root');
 
@@ -24,7 +24,7 @@ function App() {
   }
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header
         onOpenNewTransactionModal = {handleOpenNewTransactionModal}
       />
@@ -36,7 +36,7 @@ function App() {
       />
 
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
 
